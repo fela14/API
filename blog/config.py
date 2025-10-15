94be4143c0057acb6c9fb1821d3458b06e151522
fela14 <k.fakeye@yahoo.com>
2025-10-15 13:00:38 +0000

    deployment ready

---
+++ blog/config.py
@@ -0,0 +1,20 @@
+import os
+import json
+
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
+
+class Config:
+    
+    # Basic configuration
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
+
+    # Mail configuration
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = config.get('EMAIL_USER')
+    MAIL_PASSWORD = config.get('EMAIL_PASS')
+    MAIL_DEFAULT_SENDER = config.get('EMAIL_USER')
