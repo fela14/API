f51b9676ecde3936314acb2673f7306eab728606
fela <k.fakeye@yahoo.com>
2025-12-02 14:36:32 +0000

    searching

--- Django/Puddle/puddle/item/forms.py
+++ Django/Puddle/puddle/item/forms.py
@@ -29,3 +29,27 @@ class NewItemForm(forms.ModelForm):
                 'class': INPUT_CLASSES
             }),
         }
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name', 'description', 'price', 'image')
+
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES,
+                'placeholder': 'Enter item name'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES,
+                'placeholder': 'Enter item description',
+                'rows': 4
+            }),
+            'price': forms.NumberInput(attrs={
+                'class': INPUT_CLASSES,
+                'placeholder': 'Enter price'
+            }),
+            'image': forms.ClearableFileInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+        }
